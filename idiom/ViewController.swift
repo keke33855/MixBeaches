2ee0965a19b96278a77efb9c3d1548539ff7afd3
chang <465162663@qq.com>
2019-08-21 10:47:41 +0800

    Initial Commit

---
+++ idiom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  idiom
+//
+//  Created by chang on 2022/8/21.
+//  Copyright © 2019 chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
